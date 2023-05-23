9e4d1eba8933d8f1c3ec38b53defa81211fde65d
Maksym <mybusccount@gmail.com>
2023-05-23 23:56:59 +0300

    firstq

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,12 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return <>
-  <h1 className="first">Hello world</h1>
-  <a href="#" className="greed" >Hello</a>
-  <img src="" alt="oops" className="root"/>
+  <div className="container_hight">
+  <img src="./image/hightback.jpg" alt="oops" />
+  </div>
+  <div className="container_low">
+
+  </div>
   </>
   ;
 }
