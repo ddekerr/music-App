b92bde711c42a0bb11123cb6fdf25c71278a69cb
Maksym <mybusccount@gmail.com>
2023-05-25 18:38:56 +0300

    max-18:38.25/05/23

--- src/modules/auth/Auth.tsx
+++ src/modules/auth/Auth.tsx
@@ -1,27 +1,25 @@
 import "./Auth.css";
-// import profile from "../../image/pngegg.png";
+import profile from "../../image/pngegg.png";
 
 function Auth() {
   return (
     <>
     <div className="hero">
-
+    
     </div>
 
     <div className="registration">
-      <div className="registration__list container">
-        <div className="">
+        <div className="logo">
           <div className="sumbol"><img src={profile} alt="there should have been a picture but something went wrong, sorry(((" className="picture"/></div>
           <div className="greeting"><p>Time to WORSHIP</p></div>
         </div>
-        <ul>
+        <ul className="registration__list container">
           <li className="with qlog">click here fore free sing up</li>
           <li className="with">Continue with Google</li>
           <li className="with">Continue with Facebook</li>
           <li className="with">Continue with IOS</li>
           <li className="with qlog">Log in</li>
         </ul>
-      </div>
     </div>
     </>
   );
