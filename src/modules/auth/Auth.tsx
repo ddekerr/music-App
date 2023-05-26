81cb3925094550886a422b79b0359010ba6d5386
Maksym <mybusccount@gmail.com>
2023-05-26 14:34:24 +0300

    max

--- src/modules/auth/Auth.tsx
+++ src/modules/auth/Auth.tsx
@@ -1,28 +1,38 @@
 import "./Auth.css";
 import profile from "../../image/pngegg.png";
 import fonim from "../../image/555.jpg";
-
+import { Registration } from "./Auth.styled";
 
 function Auth() {
   return (
     <>
-    <div className="hero">
-      
-    </div>
+      <div className="hero"></div>
 
-    <div className="registration">
+      <Registration>
         <div className="logo">
-          <div className="sumbol"><img src={profile} alt="there should have been a picture but something went wrong, sorry(((" className="picture"/></div>
-          <div className="greeting"><p>Time to WORSHIP</p></div>
+          <img src={profile} alt="cross" className="picture" />
+          <div className="greeting">
+            <p>Time to WORSHIP</p>
+          </div>
         </div>
         <ul className="registration__list container">
-          <li className="with qlog"> <a href="#">click here fore free sing up</a></li>
-          <li className="with"> <a href="#">Continue with Google</a></li>
-          <li className="with"> <a href="#">Continue with Facebook</a></li>
-          <li className="with"> <a href="#">Continue with IOS</a></li>
-          <li className="with qlog"> <a href="#">Log in</a></li>
+          <li className="with qlog">
+            <a href="#">click here fore free sing up</a>
+          </li>
+          <li className="with">
+            <a href="#">Continue with Google</a>
+          </li>
+          <li className="with">
+            <a href="#">Continue with Facebook</a>
+          </li>
+          <li className="with">
+            <a href="#">Continue with IOS</a>
+          </li>
+          <li className="with qlog">
+            <a href="#">Log in</a>
+          </li>
         </ul>
-    </div>
+      </Registration>
     </>
   );
 }
