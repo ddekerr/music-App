abfc94a90f955b04034d7b7b156d021fbfcf7637
deker <dimon.bond.94@gmail.com>
2023-06-02 23:13:54 +0300

    work with auth state

--- src/app/auth/operations.js
+++ src/app/auth/operations.js
@@ -22,7 +22,7 @@ export const register = createAsyncThunk(
   'auth/register',
   async (data, thunkAPI) => {
     try {
-      const response = await axios.post('/auth/register');
+      const response = await axios.post('/auth/register', data);
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
@@ -30,3 +30,30 @@ export const register = createAsyncThunk(
     }
   }
 );
+
+/**
+ * POST @ /auth/login
+ * body: {email, password}
+ */
+export const login = createAsyncThunk('auth/login', async (data, thunkAPI) => {
+  try {
+    const response = await axios.post('/auth/login', data);
+    setAuthHeader(response.data.token);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+/**
+ * POST @ /auth/logout
+ * headers: Authorization: Bearer token
+ */
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/auth/logout');
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
