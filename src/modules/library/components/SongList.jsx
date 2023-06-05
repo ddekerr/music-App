0e67b1d98ae137ddb52ce16b75018cf943393c84
deker <dimon.bond.94@gmail.com>
2023-06-05 13:36:39 +0300

    remove .container from library list components

--- src/modules/library/components/SongList.jsx
+++ src/modules/library/components/SongList.jsx
@@ -1,20 +1,20 @@
-import { nanoid } from "nanoid";
-import SongItem from "./SongItem";
+import { nanoid } from 'nanoid';
+import SongItem from './SongItem';
 
 const songList = [
-  { title: "День у день", author: "Room for more" },
-  { title: "Це наш Бог", author: "Room for more" },
-  { title: "Я переможу", author: "D. Worship" },
-  { title: "Вільний", author: "Room for more" },
-  { title: "День у день", author: "Room for more" },
-  { title: "Це наш Бог", author: "Room for more" },
-  { title: "Я переможу", author: "D. Worship" },
-  { title: "Вільний", author: "Room for more" },
+  { title: 'День у день', author: 'Room for more' },
+  { title: 'Це наш Бог', author: 'Room for more' },
+  { title: 'Я переможу', author: 'D. Worship' },
+  { title: 'Вільний', author: 'Room for more' },
+  { title: 'День у день', author: 'Room for more' },
+  { title: 'Це наш Бог', author: 'Room for more' },
+  { title: 'Я переможу', author: 'D. Worship' },
+  { title: 'Вільний', author: 'Room for more' },
 ];
 
 const SongList = () => {
   return (
-    <ul className="container">
+    <ul>
       {songList.map(({ title, author }) => (
         <SongItem key={nanoid()} title={title} author={author} />
       ))}
