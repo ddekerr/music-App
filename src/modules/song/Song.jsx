dbe2951aecda3fc1338c86462973295777aa3354
Maksym <mybusccount@gmail.com>
2023-06-06 09:41:04 +0300

    max

--- src/modules/song/Song.jsx
+++ src/modules/song/Song.jsx
@@ -1,12 +1,49 @@
-import { C } from './Song.styled';
+import {
+  BackLink,
+  ForThisSong,
+  MediaContainer,
+  ContainerSong,
+  BandName,
+  SongName,
+  AlbumImage,
+  ContainerMP3,
+  ContainerMP4,
+  BorderVerse,
+  NamedBox,
+  TextBox,
+} from './Song.styled';
+import { BsArrowLeft } from 'react-icons/bs';
+import { Text } from 'modules/common';
+import { useNavigate } from 'react-router-dom';
+import AlbumFoto from '../../image/room.jpg';
+import { useEffect } from 'react';
 
 const Song = () => {
-  return <>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </>;
+  return (
+    <>
+      <BackLink>
+        <BsArrowLeft style={{ color: '#fff' }} />
+        <Text color="white">Go Back</Text>
+      </BackLink>
+
+      <ForThisSong>
+        <BandName>Room For More</BandName>
+        <SongName>Den y den</SongName>
+        <AlbumImage src={AlbumFoto} alt="" />
+      </ForThisSong>
+
+      <MediaContainer>
+        <ContainerMP4></ContainerMP4>
+        <ContainerMP3></ContainerMP3>
+      </MediaContainer>
+
+      <ContainerSong>
+        <BorderVerse></BorderVerse>
+        <NamedBox></NamedBox>
+        <TextBox></TextBox>
+      </ContainerSong>
+    </>
+  );
 };
 
 export default Song;
--- src/modules/song/Song.styled.js
+++ src/modules/song/Song.styled.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-export const C = styled.div``;
+export const BackLink = styled(NavLink)`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+`;
+
+// kirikuturasamanitarybines
+export const ForThisSong = styled.div`
+  color: white;
+  display: flex;
+`;
+
+export const BandName = styled.div``;
+
+export const SongName = styled.div``;
+
+export const AlbumImage = styled.img`
+  border-radius: 300px;
+`;
+
+// kirikuturasamanitarybines
+export const MediaContainer = styled.div``;
+
+export const ContainerMP4 = styled.div``;
+
+export const ContainerMP3 = styled.div``;
+
+// kirikuturasamanitarybines
+export const ContainerSong = styled.div``;
+
+export const BorderVerse = styled.div``;
+
+export const NamedBox = styled.p``;
+
+export const TextBox = styled.div``;
